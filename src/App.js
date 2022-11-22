038ace3f050f56c99da2ff723dfab1bb16beeed3
Гогитидзе Гиорги <90081928+gioraks@users.noreply.github.com>
2022-11-22 12:54:13 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import Remarkable from 'remarkable';
+import RemarkableReactRenderer from 'remarkable-react';
+
+
+class MarkdownEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.md = new Remarkable();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: 'Привет, **мир**!' };
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  getRawMarkup() {
+    return { __html: this.md.render(this.state.value) };
+  }
+
+  render() {
+    return (
+      <div className="MarkdownEditor">
+        <h3>Редактор</h3>
+        <label htmlFor="markdown-content">
+          Введите что-нибудь
+        </label>
+        <textarea
+          id="markdown-content"
+          onChange={this.handleChange}
+          defaultValue={this.state.value}
+        />
+        <h3>Вывод</h3>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={this.getRawMarkup()}
+        />
+      </div>
+    );
+  }
+}
+
+root.render(<MarkdownEditor />);
