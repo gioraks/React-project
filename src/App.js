3fc3c9e1725a86e32135e073e8798370644aa371
Гогитидзе Гиорги <90081928+gioraks@users.noreply.github.com>
2022-12-06 14:17:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import Remarkable from 'remarkable';
-import RemarkableReactRenderer from 'remarkable-react';
-
-
-class MarkdownEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.md = new Remarkable();
-    this.handleChange = this.handleChange.bind(this);
-    this.state = { value: 'Привет, **мир**!' };
-  }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
-
-  getRawMarkup() {
-    return { __html: this.md.render(this.state.value) };
-  }
-
-  render() {
-    return (
-      <div className="MarkdownEditor">
-        <h3>Редактор</h3>
-        <label htmlFor="markdown-content">
-          Введите что-нибудь
-        </label>
-        <textarea
-          id="markdown-content"
-          onChange={this.handleChange}
-          defaultValue={this.state.value}
-        />
-        <h3>Вывод</h3>
-        <div
-          className="content"
-          dangerouslySetInnerHTML={this.getRawMarkup()}
-        />
-      </div>
-    );
-  }
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+function Welcome(props) {
+    // let a = 1;
+    // let b = 9;
+    // let c = a + b;
+    // b +=a + b;
+    // console.log(c, b);
+    const [value, setValue] = useState();
+    return <>
+           <br/> {props.name} {value ? value.target.value : ''}
+        <input type={'text'} onChange={setValue} />
+    </>;
 }
-
-root.render(<MarkdownEditor />);
+function App() {
+    return (
+        <div>
+            <Welcome name="ФИО:" /> <br/>
+            <Welcome name="Номер телефона:" /><br />
+            <Welcome name="E-mail:" /><br />
+            <Welcome name="Дата рождения:" /><br />
+            <Welcome name="Биографические сведения:" /><br />
+        </div>   
+    );
+}
+export default Welcome; 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
